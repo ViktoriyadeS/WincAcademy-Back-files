04a17313a4f8288d2d8057c6cc1584e2a04ee5e4
Viktoriya <81090951+ViktoriyadeS@users.noreply.github.com>
2022-09-29 11:30:29 +0200

    Updated according to comments

--- files/main.py
+++ files/main.py
@@ -6,11 +6,16 @@ import os
 import shutil
 
 
+cache_path = os.path.join(os.getcwd(), 'files', 'cache')
+zip_file = os.path.abspath('files/data.zip')
+
+
 def clean_cache():
-    my_path = r'/Users/viktoriya/WincAcademy/BACK/files/cache'
-    if os.path.exists(my_path):
-        shutil.rmtree(my_path)
-    os.makedirs(my_path)
+    if os.path.exists(cache_path):
+        shutil.rmtree(cache_path)
+    else:
+        pass
+    os.makedirs(cache_path)
 
 
 def cache_zip(filename, extract_dir):
@@ -18,19 +23,29 @@ def cache_zip(filename, extract_dir):
 
 
 def cached_files():
-    my_path = os.path.abspath("files/cache")
-    dir_cached_files = os.listdir(my_path)
+    dir_cached_files = os.listdir(cache_path)
     list_cached_files = []
     for file in dir_cached_files:
-        list_cached_files.append(os.path.join(my_path, file))
+        list_cached_files.append(os.path.join(cache_path, file))
     return list_cached_files
 
 
 def find_password(list):
     for file in list:
-        with open(file, 'r') as f:
+        with open(file) as f:
             lines = f.readlines()
             for row in lines:
                 if "password" in row:
                     return row.rsplit()[-1]
         f.close()
+
+
+def main():
+    clean_cache()
+    cache_zip(zip_file, cache_path)
+    files = cached_files()
+    print(find_password(files))
+
+
+if __name__ == "__main__":
+    main()
